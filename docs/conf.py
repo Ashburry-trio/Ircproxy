5baa453cbc18ab3cf847c7cd0a1f81c3b1b71d44
David Peters <ashburryop@gmail.com>
2024-05-03 03:26:08 -0700

    Add extentions list to conf.py Maybe readthedocs will compile this time. I stil l don't know what it will look like with all the missing docstrings.

--- docs/conf.py
+++ docs/conf.py
@@ -4,7 +4,7 @@ sys.path.insert(0, os.path.abspath('.'))
 
 numfig = True
 #language = 'ja'
-
+extensions = []
 extensions.append('sphinx.ext.todo')
 extensions.append('sphinx.ext.autodoc')
 #extensions.append('sphinx.ext.autosummary')
