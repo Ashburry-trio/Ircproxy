fcd7178b6555335ad714888edc6c44a09e14d08d
David Peters <ashburryop@gmail.com>
2024-06-01 09:08:54 -0700

    turn on auto summery

--- docs/conf.py
+++ docs/conf.py
@@ -1,13 +1,13 @@
 import sys, os
 sys.path.insert(0, os.path.abspath('.'))
-#exclude_patterns = ['zzz']
+exclude_patterns = ['*.txt','.*','_build', 'Thumbs.db', '.DS_Store']
 
 numfig = True
 #language = 'ja'
 extensions=[]
 extensions.append('sphinx.ext.todo')
 extensions.append('sphinx.ext.autodoc')
-#extensions.append('sphinx.ext.autosummary')
+extensions.append('sphinx.ext.autosummary')
 extensions.append('sphinx.ext.intersphinx')
 extensions.append('sphinx.ext.mathjax')
 extensions.append('sphinx.ext.viewcode')
