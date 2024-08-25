eda40baa6babe5a1a90ee600a9cd40170199989e
Ashburry-trio <ashburryop@gmail.com>
2024-08-25 07:12:48 -0700

    removed cygwin support for now Not sure if it is needed or not. I suppose yiouw oukld use the Linux files with the folder changed to /Scripts/ instead of /bin/ I'll have to investigate this furthur.

--- activate-cygwin.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# -*- coding: utf-8 -*-
-# Type "chmod +x ./activate.sh" before running this.
-echo  "Executing activate.sh for bash shell. If you have a different shelll you need to run the appropriate activate"
-echo "script for that shell located in ./trio-ircproxy/venv/bin"
-chmod +x ./trio-ircproxy/venv/Scripts/activate.sh
-source ./trio-ircproxy/venv/Scripts/activate.sh
