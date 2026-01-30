49dff8a08dbaf289a6fdacb50353bd13c4036aee
Kenggi "David" Peters <ashburryop@gmail.com>
2026-01-29 18:18:56 -0800

    Update activate.bat

--- activate.bat
+++ activate.bat
@@ -1,11 +1,12 @@
 @echo off
 
-IF EXIST ".\trio-ircproxy\venv\Scripts\activate.bat" (
-    call ".\trio-ircproxy\venv\Scripts\activate.bat"
+IF EXIST ".\trio-ircproxy\venv\Scripts\activate.ps1" (
+    call ".\trio-ircproxy\venv\Scripts\activate.ps1"
     goto end
 )
-echo ERROR: "install.bat" must first be run after you may execute "runproxy.bat"
+echo ERROR: "install.bat" must first be run first, after you may execute "runproxy.bat"
 goto EOF
 :end
 PROMPT=(trio-ircproxy) %_OLD_VIRTUAL_PROMPT%
 :EOF
+
